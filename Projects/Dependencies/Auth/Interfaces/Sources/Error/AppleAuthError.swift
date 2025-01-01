f72fbf17fdcf172417d69d0866e625908873446e
Seongmin Hong <seongmin.hong.dev@gmail.com>
2025-01-01 19:32:09 +0900

    chore: update error definitions

--- Projects/Dependencies/Auth/Interfaces/Sources/Error/AppleAuthError.swift
+++ Projects/Dependencies/Auth/Interfaces/Sources/Error/AppleAuthError.swift
@@ -12,14 +12,22 @@ public enum AppleAuthError: LocalizedError {
     
     public var errorDescription: String? {
         switch self {
-        case .unknown:                  return "The authorization attempt failed for an unknown reason."
-        case .canceled:                 return "The user canceled the authorization attempt."
-        case .invalidResponse:          return "The authorization request received an invalid response."
-        case .notHandled:               return "The authorization request wasn’t handled."
-        case .failed:                   return "The authorization attempt failed."
-        case .notInteractive:           return "The authorization request isn’t interactive."
-        case .tokenMissing:             return "The identity token is missing"
-        case .otherError(let error):    return "The authorization error with other error: \(error.localizedDescription)"
+            case .unknown:
+                return "The authorization attempt failed for an unknown reason."
+            case .canceled:
+                return "The user canceled the authorization attempt."
+            case .invalidResponse:
+                return "The authorization request received an invalid response."
+            case .notHandled:
+                return "The authorization request wasn’t handled."
+            case .failed:
+                return "The authorization attempt failed."
+            case .notInteractive:
+                return "The authorization request isn’t interactive."
+            case .tokenMissing:
+                return "The identity token is missing"
+            case .otherError(let error):
+                return "The authorization error with other error: \(error.localizedDescription)"
         }
     }
     
@@ -29,13 +37,13 @@ public enum AppleAuthError: LocalizedError {
     
     public init(error: Error) {
         switch (error as NSError).code {
-        case 1000:      self = .unknown
-        case 1001:      self = .canceled
-        case 1002:      self = .invalidResponse
-        case 1003:      self = .notHandled
-        case 1004:      self = .failed
-        case 1005:      self = .notInteractive
-        default:        self = .otherError(error)
+            case 1000:      self = .unknown
+            case 1001:      self = .canceled
+            case 1002:      self = .invalidResponse
+            case 1003:      self = .notHandled
+            case 1004:      self = .failed
+            case 1005:      self = .notInteractive
+            default:        self = .otherError(error)
         }
     }
 }
