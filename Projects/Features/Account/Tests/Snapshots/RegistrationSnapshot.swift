9272691513378349beb9c2ef729d523209b3cc62
Seongmin Hong <seongmin.hong.dev@gmail.com>
2025-01-01 23:25:25 +0900

    test: add snapshot test for accout

---
+++ Projects/Features/Account/Tests/Snapshots/RegistrationSnapshot.swift
@@ -0,0 +1,44 @@
+//
+//  AccountSnapshot.swift
+//  AccountTest
+//
+//  Created by Sean Hong on 1/1/25.
+//
+
+import ComposableArchitecture
+import SnapshotTesting
+import XCTest
+import SwiftUI
+
+@testable import Account
+
+
+final class RegistrationSnapshot: XCTestCase {
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testRegistrationSnapshot() throws {
+        let view = RegistrationScreen(
+            store: StoreOf<Registration>(
+                initialState: Registration.State(),
+                reducer: { Registration() },
+                withDependencies: {
+                    $0.authClient = .testValue
+                }
+            )
+        )
+        let vc = UIHostingController(rootView: view)
+        vc.view.frame = UIScreen.main.bounds
+        assertSnapshot(of: vc, as: .image(on: .iPhoneSe))
+        assertSnapshot(of: vc, as: .image(on: .iPhone13))
+        assertSnapshot(of: vc, as: .image(on: .iPhone13Mini))
+        assertSnapshot(of: vc, as: .image(on: .iPhone13Pro))
+        assertSnapshot(of: vc, as: .image(on: .iPhone13ProMax))
+    }
+}
