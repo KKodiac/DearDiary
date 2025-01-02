a59413fc4bf3e9ed82ef036752be9fa72cb720ea
Sean Hong(홍성민) <35219323+KKodiac@users.noreply.github.com>
2025-01-02 22:51:19 +0900

    Snapshot Testing for Account Feature (#3)
    * chore: add swift-snapshot-testing
    
    * feature: add testable implementation for firebase
    
    * test: add snapshot test for accout
    
    * chore: add new snapshots
    
    ---------
    
    Co-authored-by: Seongmin Hong <seongmin.hong.dev@gmail.com>

---
+++ Projects/Features/Account/Tests/Snapshots/AuthenticationSnapshot.swift
@@ -0,0 +1,46 @@
+//
+//  AccountSnapshot.swift
+//  AccountTest
+//
+//  Created by Sean Hong on 1/1/25.
+//
+
+import ComposableArchitecture
+import SnapshotTesting
+import XCTest
+import SwiftUI
+
+@testable import Account
+
+
+final class AuthenticationSnapshot: XCTestCase {
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testAuthenticationSnapshot() throws {
+        let view = AuthenticationScreen(
+            store: StoreOf<Authentication>(
+                initialState: Authentication.State(
+                    clientUID: Shared("")
+                ),
+                reducer: { Authentication() },
+                withDependencies: {
+                    $0.authClient = .testValue
+                }
+            )
+        )
+        let vc = UIHostingController(rootView: view)
+        vc.view.frame = UIScreen.main.bounds
+        assertSnapshot(of: vc, as: .image(on: .iPhoneSe))
+        assertSnapshot(of: vc, as: .image(on: .iPhone13))
+        assertSnapshot(of: vc, as: .image(on: .iPhone13Mini))
+        assertSnapshot(of: vc, as: .image(on: .iPhone13Pro))
+        assertSnapshot(of: vc, as: .image(on: .iPhone13ProMax))
+    }
+}
